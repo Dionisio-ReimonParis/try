0f6a254c2322794396fcb460305fcc1c2f8906c6
Dionisio-ReimonParis <dionisio.rparis00133@dyci.edu.ph>
2024-12-27 03:31:32 +0800

    name

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1>Welcome to React</h1>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
+			<div className="modern-container">
+				<h1 className="paris-text">I am Paris</h1>
+			</div>
 		</div>
 	);
 }
