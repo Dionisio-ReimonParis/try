98dd876e6659621bb49ffaa6c14ce5e5e66a77be
Dionisio-ReimonParis <dionisio.rparis00133@dyci.edu.ph>
2024-12-27 03:56:01 +0800

    fix design + link to fb

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
+	const handleClick = () => {
+		window.open('https://www.facebook.com/Nomierparis/', '_blank');
+	};
+
 	return (
 		<div className="App">
-			<div className="modern-container">
+			<div className="modern-container" onClick={handleClick}>
 				<h1 className="paris-text">I am Paris</h1>
 			</div>
 		</div>
